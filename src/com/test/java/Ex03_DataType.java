ca81d5fd218cea3860690b9e2de847ba6cdc3602
2uiwnsx <2uiwnsx@gmail.com>
2024-01-04 15:47:59 +0900

    Add: Ex03_DataType.java 파일 추가

---
+++ src/com/test/java/Ex03_DataType.java
@@ -0,0 +1,25 @@
+/*
+	▶ 변수 네이밍 규칙
+	
+		▷ 숫자, 영문자, '_'를 사용할 수 있다.
+		▷ 숫자가 아닌 문자로 시작해야 한다.
+		▷ 공백을 포함할 수 없다.
+		▷ 예약어를 사용할 수 없다.
+		▷ 대소문자를 구분한다.
+		
+	▶ 변수명은 별칭일 뿐, 실제 메모리 주소는 숫자로 저장된다.
+*/
+
+package com.test.java;
+
+public class Ex03_DataType {
+	
+	public static void main(String[] args) {
+		
+		byte koreanScore = 100;
+		
+		System.out.println(koreanScore);
+		
+	}
+
+}
