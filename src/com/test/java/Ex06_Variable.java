a24f8f4dacf1f49c9aba9b7389bc6955c720072a
2uiwnsx <2uiwnsx@gmail.com>
2024-01-04 15:48:57 +0900

    Add: Ex06_Variable.java 파일 추가

---
+++ src/com/test/java/Ex06_Variable.java
@@ -0,0 +1,324 @@
+package com.test.java;
+
+public class Ex06_Variable {
+	
+	public static void main(String[] args) {
+		
+		// 논리형
+		
+		// boolean
+		
+		System.out.println("----- boolean -----\n");
+		
+		boolean answer1 = false;
+		System.out.println("Q. 사과는 노란색입니다. ➜ " + answer1);
+
+		boolean answer2 = true;
+		System.out.println("Q. 거미의 다리는 8개입니다. ➜ " + answer2);
+
+		boolean answer3 = true;
+		System.out.println("Q. 승용차의 바퀴는 4개입니다. ➜ " + answer3);
+
+		boolean answer4 = false;
+		System.out.println("Q. 양파는 과일입니다. ➜ " + answer4);
+
+		boolean answer5 = true;
+		System.out.println("Q. 대한민국의 수도는 서울입니다. ➜ " + answer5);
+
+		boolean answer6 = false;
+		System.out.println("Q. 치킨은 건강식품입니다. ➜ " + answer6);
+
+		boolean answer7 = false;
+		System.out.println("Q. 판다는 파충류입니다. ➜ " + answer7);
+
+		boolean answer8 = false;
+		System.out.println("Q. 지구는 평평합니다. ➜ " + answer8);
+
+		boolean answer9 = true;
+		System.out.println("Q. 사이다는 탄산음료입니다. ➜ " + answer9);
+
+		boolean answer10 = true;
+		System.out.println("Q. 코스모스는 꽃입니다. ➜ " + answer10);
+		
+		// 문자형
+		
+		// char
+		
+		System.out.println("\n----- char -----\n");
+		
+		char monetaryUnitInKorea = '￦';
+		System.out.println("대한민국의 통화 기호는 " + monetaryUnitInKorea + "입니다.");
+
+		char monetaryUnitInGermany = '€';
+		System.out.println("독일의 통화 기호는 " + monetaryUnitInGermany + "입니다.");
+
+		char monetaryUnitInChina = '¥';
+		System.out.println("중국의 통화 기호는 " + monetaryUnitInChina + "입니다.");
+
+		char monetaryUnitInUnitedStates = '$';
+		System.out.println("미국의 통화 기호는 " + monetaryUnitInUnitedStates + "입니다.");
+
+		char favoriteAnimal = '곰';
+		System.out.println("제가 좋아하는 동물은 " + favoriteAnimal + "입니다.");
+
+		char leastFavoriteAnimal = '쥐';
+		System.out.println("제가 싫어하는 동물은 " + leastFavoriteAnimal + "입니다.");
+
+		char firstLetterOfTheAlphabet = 'A';
+		System.out.println("알파벳의 첫 번째 글자는 " + firstLetterOfTheAlphabet + "입니다.");
+
+		char lastLetterOfTheAlphabet = 'Z';
+		System.out.println("알파벳의 마지막 글자는 " + lastLetterOfTheAlphabet + "입니다.");
+
+		char favoriteFood = '회';
+		System.out.println("제가 좋아하는 음식은 " + favoriteFood + "입니다.");
+
+		char leastFavoriteFood = '국';
+		System.out.println("제가 싫어하는 음식은 " + leastFavoriteFood + "입니다.");
+
+		// 정수형
+		
+		// byte
+		
+		System.out.println("\n----- byte -----\n");
+		
+		byte age = 24;
+		System.out.println("저는 " + age + "살입니다.");
+		
+		byte day = 17;
+		System.out.println("오늘은 " + day + "일입니다.");
+		
+		byte humidity = 82;
+		System.out.println("현재 수원시의 습도는 " + humidity + "%입니다.");
+		
+		byte tabletBattery = 67;
+		System.out.println("제 태블릿 배터리는 " + tabletBattery + "%입니다.");
+		
+		byte theNumberOfFingers = 10;
+		System.out.println("제 손가락은 " + theNumberOfFingers + "개입니다.");
+		
+		byte nextMonth = 8;
+		System.out.println("다음 달은 " + nextMonth + "월입니다.");
+		
+		byte grade = 4;
+		System.out.println("저는 " + grade + "학년입니다.");
+		
+		byte theNumberOfPens = 6;
+		System.out.println("저는 볼펜 " + theNumberOfPens + "자루를 갖고 있습니다.");
+		
+		byte theNumberOfStatesInTheUnitedStates = 50;
+		System.out.println("미국에는 " + theNumberOfStatesInTheUnitedStates + "개의 주가 있습니다.");
+		
+		byte assignmentScores = 100;
+		System.out.println("제 과제 점수는 " + assignmentScores + "점입니다.");
+		
+		// short
+		
+		System.out.println("\n----- short -----\n");
+		
+		short footSize = 230;
+		System.out.println("제 발 사이즈는 " + footSize + "mm입니다.");
+		
+		short height = 180;
+		System.out.println("제 키는 " + height + "cm입니다.");
+		
+		short minimumWage = 9620;
+		System.out.printf("대한민국의 최저임금은 %,d원입니다.\n", minimumWage);
+		
+		short lengthOfTheNile = 6650;
+		System.out.printf("나일강의 길이는 %,dkm입니다.\n", lengthOfTheNile);
+		
+		short theNumberOfIslands = 4198;
+		System.out.printf("대한민국에는 %,d개의 섬이 있습니다.\n", theNumberOfIslands);
+		
+		short caloriesOfCoke = 216;
+		System.out.println("콜라 한 병은 " + caloriesOfCoke + "kcal입니다.");
+		
+		short theNumberOfCountriesOnTheWorldMap = 237;
+		System.out.println("세계지도에는 " + theNumberOfCountriesOnTheWorldMap + "개의 국가가 표시되어 있습니다.");
+		
+		short circumferenceOfTheMoon = 10921;
+		System.out.printf("달의 둘레는 %,dkm입니다.\n", circumferenceOfTheMoon);
+
+		short theNumberfOfStarbucks = 1800;
+		System.out.printf("대한민국에는 %,d개의 스타벅스 매장이 있습니다.\n", theNumberfOfStarbucks);
+
+		short populationDensityInHongKong = 6849;
+		System.out.printf("홍콩의 인구밀도는 %,d명/km²입니다.\n", populationDensityInHongKong);
+		
+		// int
+		
+		System.out.println("\n----- int -----\n");
+		
+		int priceOfGrandeur = 37430000;
+		System.out.printf("그랜저는 %,d원입니다.\n", priceOfGrandeur);
+
+		int circumferenceOfTheEarth = 40075;
+		System.out.printf("지구 둘레는 %,dkm입니다.\n", circumferenceOfTheEarth);
+		
+		int frenchPopulation = 64756584;
+		System.out.printf("프랑스의 총인구는 %,d명입니다.\n", frenchPopulation);
+		
+		int koreanPopulation = 51558034;
+		System.out.printf("대한민국의 총인구는 %,d명입니다.\n", koreanPopulation);
+		
+		int areaOfChina = 9640821;
+		System.out.printf("중국의 총면적은 %,dkm²입니다.\n", areaOfChina);
+		
+		int areaOfCanada = 9984670;
+		System.out.printf("캐나다의 총면적은 %,dkm²입니다.\n", areaOfCanada);
+		
+		int bankBalance = 15000000;
+		System.out.printf("제 통장 잔고는 %,d원입니다.\n", bankBalance);
+
+		int lengthOfTheCoastlineOfAsia = 129077;
+		System.out.printf("아시아의 해안선 길이는 %,dkm입니다.\n", lengthOfTheCoastlineOfAsia);
+
+		int theNumberOfArabicSpeakers = 290000000;
+		System.out.printf("전 세계 아랍어 사용자는 %,d명입니다.\n", theNumberOfArabicSpeakers);
+		
+		int theNumberOfNewbornsPerYear = 260000;
+		System.out.printf("대한민국의 연간 출생아는 %,d명입니다.\n", theNumberOfNewbornsPerYear);
+		
+		// long
+		
+		System.out.println("\n----- long -----\n");
+		
+		long worldPopulation = 8045311447L;
+		System.out.printf("세계 인구는 %,d명입니다.\n", worldPopulation);
+
+		long asiaPopulation = 4751819588L;
+		System.out.printf("아시아 대륙의 인구는 %,d명입니다.\n", asiaPopulation);
+
+		long nominalGdp = 2161773900000000L;
+		System.out.printf("대한민국의 명목 GDP는 %,d원입니다.\n", nominalGdp);
+
+		long realGdp = 1968839500000000L;
+		System.out.printf("대한민국의 실질 GDP는 %,d원입니다.\n", realGdp);
+
+		long nationalDebtOfTheRepublicOfKorea = 970700000000000L;
+		System.out.printf("대한민국의 국채는 %,d원입니다.\n", nationalDebtOfTheRepublicOfKorea);
+
+		long nominalGdi = 2193527500000000L;
+		System.out.printf("대한민국의 명목 GDI는 %,d원입니다.\n", nominalGdi);
+
+		long grdp = 2076254437000000L;
+		System.out.printf("대한민국의 GRDP는 %,d원입니다.\n", grdp);
+
+		long marketCapitalizationOfApple = 3854821300000000L;
+		System.out.printf("애플의 시가총액은 %,d원입니다.\n", marketCapitalizationOfApple);
+
+		long marketCapitalizationOfTesla = 1152453000000000L;
+		System.out.printf("테슬라의 시가총액은 %,d원입니다.\n", marketCapitalizationOfTesla);
+
+		long marketCapitalizationOfAmazon = 1741635900000000L;
+		System.out.printf("아마존의 시가총액은 %,d원입니다.\n", marketCapitalizationOfAmazon);
+
+		// 실수형
+		
+		// float
+		
+		System.out.println("\n----- float -----\n");
+		
+		float laptopSize = 13.3F;
+		System.out.println("제 노트북은 " + laptopSize + "inch입니다.");
+		
+		float temperature = 25.8F;
+		System.out.println("현재 성남시의 온도는 " + temperature + "°입니다.");
+		
+		float employmentRate = 63.5F;
+		System.out.println("대한민국의 고용률은 " + employmentRate + "%입니다.");
+		
+		float baseRate = 3.50F;
+		System.out.println("대한민국의 기준금리는 " + baseRate + "%입니다.");
+		
+		float lifeExpectancyAtBirth = 83.6F;
+		System.out.println("대한민국의 기대수명은 " + lifeExpectancyAtBirth + "세입니다.");
+		
+		float heightOfLotteWorldTower = 554.5F;
+		System.out.println("롯데월드타워의 높이는 " + heightOfLotteWorldTower + "m입니다.");
+		
+		float eyesight = 1.0F;
+		System.out.println("제 시력은 " + eyesight + "입니다.");
+		
+		float weight = 64.5F;
+		System.out.println("제 몸무게는 " + weight + "kg입니다.");
+		
+		float averageHeightOfWomen = 161.77F;
+		System.out.println("대한민국 여자 평균 키는 " + averageHeightOfWomen + "cm입니다.");
+		
+		float annualPrecipitation = 1244.5F;
+		System.out.println("대한민국 연강수량은 " + annualPrecipitation + "mm입니다.");
+		
+		// double
+		System.out.println("\n----- double -----\n");
+		
+		double height0fHallaMountain = 1947.269341;
+		System.out.println("한라산의 높이는 " + height0fHallaMountain + "m입니다.");
+		
+		double pi = 3.1415926535;
+		System.out.println("원주율은 " + pi + "입니다.");
+		
+		double bmi = 16.934434534534;
+		System.out.println("제 bmi는 " + bmi + "입니다.");
+		
+		double calculationResult = 3.3333333;
+		System.out.println("10 ÷ 3 = " + calculationResult + "입니다.");
+		
+		double infiniteDecimal = 1.41421356237309;
+		System.out.println("√2 = " + infiniteDecimal + "입니다.");
+		
+		double holding = 1.3234537;
+		System.out.println("저는 알파벳의 주식을 " + holding + "주 보유하고 있습니다.");
+		
+		double areaOfJejuIsland = 1850.28;
+		System.out.printf("제주도의 면적은 %,.2fk㎡  입니다.\n", areaOfJejuIsland);
+		
+		double weightOfElephant = 5000.5326;
+		System.out.printf("코끼리의 평균 몸무게는 %,fkg 입니다.\n", weightOfElephant);
+
+		double precipitation = 60.248373;
+		System.out.println("오늘의 강수량은  " + precipitation + "mm입니다.");
+
+		double perimeterOfSchoolGround = 425.29481;
+		System.out.println("학교 운동장의 둘레는  " + perimeterOfSchoolGround + "m입니다.");
+		
+		// 문자열
+		
+		// string
+		
+		System.out.println("\n----- string -----\n");
+		
+		String countryCodeOfJapan = "JP";
+		System.out.println("일본의 국가코드는 " + countryCodeOfJapan + "입니다.");
+		
+		String nationalFlowerOfSpain = "카네이션";
+		System.out.println("스페인의 국화는 " + nationalFlowerOfSpain + "입니다.");
+		
+		String stateReligionOfGreece = "정교회";
+		System.out.println("그리스의 국교는 " + stateReligionOfGreece + "입니다.");
+		
+		String officialLanguageOfBrazil = "포르투갈어";
+		System.out.println("브라질의 공용어는 " + officialLanguageOfBrazil + "입니다.");
+		
+		String capitalOfRussia = "모스크바";
+		System.out.println("러시아의 수도는 " + capitalOfRussia + "입니다.");
+		
+		String name = "차수민";
+		System.out.println("제 이름은 " + name + "입니다.");
+		
+		String major = "경영정보";
+		System.out.println("제 전공은 " + major + "입니다.");
+		
+		String address = "경기도";
+		System.out.println("저는 " + address + "에 거주합니다.");
+		
+		String hobby = "수영";
+		System.out.println("제 취미는 " + hobby + "입니다.");
+		
+		String season = "여름";
+		System.out.println("7월은 " + season + "입니다.");
+		
+	}
+
+}
